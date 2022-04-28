ce8d75c29a6726d52876b4e0ddb819177ee6e3fb
Tiago <ztiagok@gmail.com>
2022-04-28 17:24:05 -0500

    c2 - Conclusão de mais requisitos

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const iFuncionamento = document.querySelector('#funcionamento')
 const iTextoTarefa = document.querySelector('#texto-tarefa')
 const iListaTarefas = document.querySelector('#lista-tarefas')
 const iCriarTarefa = document.querySelector('#criar-tarefa')
+const iApagarTudo = document.querySelector('#apaga-tudo')
+const iConcluir = document.querySelector('#remover-finalizados')
 
 
 // MAIN
@@ -24,7 +26,9 @@ function createItem()
 
   window.cObjetoLista = child
   window.cObjetoLista.addEventListener('click', changeItem) 
+  window.cObjetoLista.addEventListener('dblclick', finishItem)
 }
+
 
 function changeItem(origin)
 {
@@ -34,14 +38,77 @@ function changeItem(origin)
   for (let index = 0; index < element.length; index += 1)
   {
 
-    element[index].className = 'list-object'
-  }
+    if (element[index].className.includes('selected'))
+    {
 
-  origin.target.className = 'list-object selected'
+      element[index].className = 'list-object'
+    }
+  }
+  
+
+  if (origin.target.className.includes('completed'))
+  {
+
+    origin.target.className = 'list-object selected completed'
+  }
+  else if (origin.target.className.includes('selected completed'))
+  {
+
+    origin.target.className = 'list-object completed'
+  }
+  else
+  {
+
+    origin.target.className = 'list-object selected'
+  }
+}
+
+
+function finishItem(origin)
+{
+
+  origin.target.className = 'list-object completed'
+
+  origin.target.removeEventListener('dblclick', finishItem)
+  origin.target.addEventListener('dblclick', unfinishItem)
+}
+
+
+function unfinishItem(origin)
+{
+
+  origin.target.className = 'list-object'
+
+  origin.target.removeEventListener('dblclick', unfinishItem)
+  origin.target.addEventListener('dblclick', finishItem)
+}
+
+function deleteEverything()
+{
+
+  const element = document.querySelector('#lista-tarefas')
+
+  while (element.lastChild)
+  {
+
+    element.removeChild(element.lastChild)
+  }
+}
+
+function deleteFinished()
+{
+  const element = document.querySelectorAll('#lista-tarefas .completed')
+
+  for (let index = 0; index < element.length; index += 1)
+  {
+
+    element[index].remove()
+  }
 
 }
 
 
 // EVENT LISTENERS
 iCriarTarefa.addEventListener('click', createItem)
-
+iApagarTudo.addEventListener('click', deleteEverything)
+iConcluir.addEventListener('click', deleteFinished)
